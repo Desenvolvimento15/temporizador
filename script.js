d69bc73cae7e2978520e18ef9341612e7f2b7219
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2022-08-25 11:31:47 -0300

    vs01

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 // Define a data para a qual estamos em contagem regressiva
-  var countDownDate = new Date("August 25, 2022 18:00:00").getTime();
-
+var countDownDate = new Date("August 25, 2022 18:00:00").getTime();
 // Atualiza a contagem regressiva a cada 1 segundo
   var x = setInterval(function() {
-
 // Obtém a data e hora de hoje
   var now = new Date().getTime();
-
 // Encontra a distância entre agora e a data da contagem regressiva
   var distance = countDownDate - now;
-
 // Cálculos de tempo para dias, horas, minutos e segundos
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+  if(hours < 10){
+    hours = '0'+hours;
+  }
+     if(minutes < 10){
+    minutes = '0'+minutes;
+  }
+    if(seconds < 10){
+    seconds = '0'+seconds;
+  }
+    
 // Exibe o resultado no elemento com id="timer"
-  document.getElementById("timer").innerHTML = days + "D " + hours + "H"
-  + minutes + "M" + seconds + "S";
+    days = days > 0 ? days+"D ": "0D ";
+document.getElementById("timer").innerHTML =   days + hours + ":"+ minutes + ":" + seconds;
 // Se a contagem regressiva terminar, escreva algum texto
   if (distance < 0) {
     clearInterval(x);
